ef472f3f4d365a4cdbaf3070a59b2c132e125535
√ｲﾶﾑム <113216701+magnusekvojtech@users.noreply.github.com>
2023-02-22 12:05:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Proměnné
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, jsem tvůj elektronický papoušek♥");
+            Console.WriteLine("Napiš co mám zvopakovat☺");
+            string k_opakovani = Console.ReadLine();
+            string zopakovano;
+            zopakovano = k_opakovani + "," + k_opakovani + "!";
+            Console.WriteLine(zopakovano);
+            Console.ReadKey();
+
+        }
+    }
+}
